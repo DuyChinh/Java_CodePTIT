8bec9f907f5b2fd1b65e6cfddf34fa5dcee3bd30
DuyChinh <doanduychinh2102@gmail.com>
2023-09-16 16:59:30 +0700

    exe 1

--- Thuc hanh/EXE3.java
+++ Thuc hanh/EXE3.java
@@ -1,23 +1,50 @@
 package EXE;
 import java.util.*;
-public class EXE5 {
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		String s = sc.nextLine();
-		int cntTwo = 0, cntSix = 0;
-		for(int i = 0; i < s.length(); i++) {
-			if(s.charAt(i) == '2') {
-				cntTwo++;
-			} 
-			if(s.charAt(i) == '6') {
-				cntSix++;
+import java.util.Scanner;
+
+
+
+public class EXE1 {
+	public static boolean isPrime(int n) {
+		if(n < 2) {
+			return false;
+		}
+		for(int i = 2; i <= Math.sqrt(n); i++ ) {
+			if(n % i == 0) {
+				return false;
 			}
 		}
-		int cnt = cntTwo + cntSix;
-		if(cnt == 2 || cnt == 6) {
-			System.out.println("YES");
-		} else {
-			System.out.println("NO");
+		return true;
+	}
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int t = sc.nextInt();
+		int cnt = 1;
+		while(t-->0) {
+			
+			int n = sc.nextInt();
+			int m = sc.nextInt();
+			int[][] arr = new int[n][m];
+			for(int i = 0; i < n; i++) {
+				for(int j = 0; j < m; j++) {
+					arr[i][j] = sc.nextInt();
+				}
+			}
+			System.out.println("Test "+ cnt++ + ":");
+			boolean check = true;
+			for(int i = 0; i < n; i++) {
+				check = true;
+				for(int j = 0; j < m; j++) {
+					if(!isPrime(arr[i][j]) ) {
+						check = false;
+						break;
+					}
+				}
+				if(check) {
+					System.out.print(i + " ");
+				}
+			}
+			System.out.println();
 		}
 	}
 }
